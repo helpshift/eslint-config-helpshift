716a357f27114f9866437b461b295915e3d800ac
Ayush Sachdeva <ayush.sachdeva@helpshift.com>
2021-05-13 15:28:44 +0530

    Revert "Update babel eslint parser"
    Upgrading to @babel/eslint-parser requires an update in babel package in
    consumer repo. So it is a breaking change. Reverting this for now so
    that this update can be addressed separately.
    
    This reverts commit 8fe96e59405586e43d89f2fff0341a2d6ecdef5c.
    
    Change-Id: I8d53915cbff09d79a06dc5ce41ddb414a5da872e

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   "plugins": ["react", "react-hooks", "prettier"],
-  "parser": "@babel/eslint-parser",
+  "parser": "babel-eslint",
   "parserOptions": {
     "sourceType": "script"
   },
