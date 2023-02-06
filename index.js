98e28d60c8cfaa5ffe9bd88b43f583b953e834a2
saunved <saunved.mutalik@helpshift.com>
2023-02-06 17:58:04 +0530

    Move prettier rules into the rules folder
    Change-Id: Idccffaf5075a305fc329d3c6b20baf13bc64ea23

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ module.exports = {
       version: "16.10.2",
     },
   },
+  // Refer: https://eslint.org/docs/rules/<rule-name>
+  // for more information on each rule.
+  // E.g. https://eslint.org/docs/rules/no-cond-assign
   extends: [
     "./rules/errors",
     "./rules/best-practices",
@@ -17,17 +20,12 @@ module.exports = {
     "./rules/stylistic",
     "./rules/es6",
     "./rules/jsx",
+    "./rules/prettier",
   ].map(require.resolve),
   env: {
     browser: true,
     amd: true,
     es6: true,
   },
-  rules: {
-    "prettier/prettier": "error",
-
-    // Refer: https://eslint.org/docs/rules/<rule-name>
-    // for more information on each rule.
-    // E.g. https://eslint.org/docs/rules/no-cond-assign
-  },
+  rules: {},
 };
